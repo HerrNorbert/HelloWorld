dffa477f25fe0ea1e26aa26c6e09d6f0594e1579
herrnorbert <nkobli90@gmail.com>
2016-12-14 18:25:29 +0100

    new class Wishitem extends basicItem

---
+++ TabbedPageWithFragments/app/src/main/java/hu/gdf/norbi/tabbedpagewithfragments/items/WishItem.java
@@ -0,0 +1,35 @@
+package hu.gdf.norbi.tabbedpagewithfragments.items;
+
+/**
+ * Created by Norbi on 2016. 12. 13..
+ */
+
+public class WishItem extends BasicItem {
+    private boolean gotIt;
+//    private CheckBox cb;
+
+    public WishItem() {
+        this.gotIt = false;
+//        this.cb=null;
+    }
+
+    public WishItem(String name) {
+        super(name);
+        this.gotIt = false;
+//        this.cb=null;
+    }
+
+    public WishItem(String name, String description) {
+        super(name, description);
+        this.gotIt = false;
+//        this.cb=null;
+    }
+
+    public boolean isGotIt() {
+        return gotIt;
+    }
+
+    public void setGotIt(boolean gotIt) {
+        this.gotIt = gotIt;
+    }
+}
